733b57589b87514a481f1b8be32d6352774be01a
Vladimir Vinogradov <vinogradovve@gmail.com>
2017-01-17 11:47:50 +0500

    Fixed load native crypto sh script

--- build/go-get-virgil-crypto.sh
+++ build/go-get-virgil-crypto.sh
@@ -5,10 +5,10 @@ go get -v ./...
 pwd
 # until crypto-go wrapper is not published
 cd $GOPATH
-mkdir -p $GOPATH/src/github.com/VirgilSecurity/virgil-crypto-go
+mkdir -p $GOPATH/src/gopkg.in/virgilsecurity/virgil-crypto-go.v4
 wget cdn.VirgilSecurity.com/crypto-go/crypto-go-linux.tgz
-tar -xvf crypto-go-linux.tgz --strip-components=1 -C $GOPATH/src/github.com/VirgilSecurity/virgil-crypto-go/
+tar -xvf crypto-go-linux.tgz --strip-components=1 -C $GOPATH/src/gopkg.in/virgilsecurity/virgil-crypto-go.v4/
 cd -
-echo "listing: $GOPATH/src/github.com/VirgilSecurity/virgil-crypto-go/"
-ls -l $GOPATH/src/github.com/VirgilSecurity/virgil-crypto-go/
+echo "listing: $GOPATH/src/gopkg.in/virgilsecurity/virgil-crypto-go.v4/"
+ls -l $GOPATH/src/gopkg.in/virgilsecurity/virgil-crypto-go.v4/
 gcc -v
